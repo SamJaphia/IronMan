9ef267d85d90b361241f52ead93cd04adb5f066b
dennis michael <denniskilawilla@gmail.com>
2020-09-08 13:10:50 +0300

    several file changes

--- op.js
+++ op.js
@@ -1,11 +1,10 @@
-const canvas = document.getElementById("canvas1")
+const canvas = document.getElementById("gameCanvas")
 const context = canvas.getContext('2d')
-canvas.width = 3000;
-canvas.height = 2000;
+canvas.width = 2000;
+canvas.height = 1000;
 
 var height = document.documentElement.clientHeight;
 var width = document.documentElement.clientWidth;
-
 const keys =[];
 
 const player = {
@@ -37,14 +36,29 @@ this.map = [
 
 
 
+
+
+
 const playerSprite = new Image();
-playerSprite.src="./Images/Good one121.png"
+playerSprite.src="./Images/g_one121.png"
 const background = new Image();
 
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     context.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH)
 }
+
+background.src = "Images/background6.png";
+
+
+ /**
+ * Main function of the game
+ * called repeatedly to advance the game
+ */
+function main() {
+    startAnimating(10);    
+ }
+
 
 window.addEventListener("keydown", function(e){
     keys[e.keyCode] = true;
@@ -56,6 +70,7 @@ window.addEventListener("keyup", function(e) {
     delete keys[e.keyCode]
     player.moving = false
 });
+
 
 function movePlayer() {
     if (keys[38] && player.y > 100) {
@@ -86,32 +101,6 @@ function handlePlayerFrame(){
     else player.frameX = 0;
 }
 
-var tile_pic = new Image();
-function loop() {
-
-    window.requestAnimationFrame(loop)
-    var height = document.documentElement.clientHeight;
-    var width = document.documentElement.clientWidth;
-
-    context.canvas.height = height;
-    context.canvas.width = width;
-
-tile_pic.addEventListener("load", (event)=> {loop(); });
-console.log(tile_pic)
-
-tile_pic.src = "Images/Background6.png";
-
-
-
-for (let x = 0; x < columns; x++){
-    for (let y = 0; y < rows; y++){
-        let value =map[y * columns + x];
-        let tile_x = x * tile_size;
-        let tile_y = y* tile_size;
-        context.drawImage(tile_pic, value * tile_size, 0, tile_size, tile_size, tile_x, tile_y, player.x * 3, player.y * 3)
-    }
-}
-}
 
 let fps, fpsInterval, startTime, now, then, elapsed;
 
@@ -122,19 +111,18 @@ function startAnimating(fps){
     animate();
 }
 
+
+
 function animate(){
-    requestAnimationFrame(animate);
+    requestAnimationFrame(animate);    
     now = Date.now();
     elapsed = now -then;
     if(elapsed > fpsInterval){
         then = now -(elapsed % fpsInterval);
-        context.clearRect(0,0,canvas.width, canvas.height);
-        context.drawImage(background, 0 ,0, 600, 600);
         drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width * 3, player.height * 3);
         movePlayer();
         handlePlayerFrame();
     }
 }
 
-startAnimating(10);
-
+main()
